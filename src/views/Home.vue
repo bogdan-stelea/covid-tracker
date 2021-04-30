13998bfa397b7879f480e334f062188f70384294
Bogdan Stelea <bogdan.steleav@gmail.com>
2021-04-30 20:13:34 +0300

    Added global covid cases, selector for each country and clear country

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,15 @@
         :text="title"
         :data="dataDate"
     />
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData"
+                   :countries="countries" />
+    <button @click="clearCountryData"
+            v-if="stats.Country"
+            class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"> Clear country</button>
   </main>
+
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
       Fetching Data
@@ -15,11 +23,15 @@
 
 <script>
 import DataTitle from "../components/DataTitle";
+import DataBoxes from "../components/DataBoxes";
+import CountrySelect from "../components/CountrySelect";
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
+    DataBoxes,
+    CountrySelect
   },
   data() {
     return {
@@ -36,6 +48,17 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
